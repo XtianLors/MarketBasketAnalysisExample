bdfa39cb1d7a42d4373d4edcb3675ae6aac32145
XtianLors <crisdalori@gmail.com>
2024-06-20 14:08:37 -0600

    Added function to paste dataset information, data types are coherent with dplyr. we still need to do the exploratory data analysis.

--- initialAnalysis.R
+++ initialAnalysis.R
@@ -6,14 +6,49 @@
 
 library(arules)
 library(dplyr)
+source("./path_value.R")
 
-my_data <- read.csv(".\\archive\\transIDitemCODE.csv")
+readerOfData <- function(path, spitter = FALSE )
+{
+
+   insiderData <- read.csv(path)
+  
+   if(spitter)
+   {
+      values <- paste("Type of data: ", typeof(insiderData), "\n",
+        "Structure of the data:  ", str(insiderData), "\n",
+        "Dimensions of the data: ", dim(insiderData), "\n",
+        "Class of the data: ", class(insiderData), "\n"
+      )
+   }
+   return(paste(values, head(insiderData), tail(insiderData))) 
+}
+readerOfData(rawDataPath, spitter = TRUE)
+
+full_data <- read.csv(rawDataPath)
+typeof(full_data)
+str(full_data)
+dim(full_data)
+class(full_data)
+
+head(full_data)
+tail(full_data)
+
+retailData <- as_tibble(my_data)
+
+my_data <- read.csv(path)
+typeof(my_data)
+str(my_data)
+dim(my_data)
+class(my_data)
 
 head(my_data)
 
-summarize(group_by(my_data, "TransactionId"), order.count = n_distinct(TransactionId))
+retailData <- as_tibble(my_data)
 
-summarize(group_by(my_data, "ItemCode"), order.count = n_distinct(ItemCode))
+summarize(group_by(retailData, "TransactionId"), order.count = n_distinct(TransactionId))
+
+summarize(group_by(retailData, "ItemCode"), order.count = n_distinct(ItemCode))
 
 d_fromcsv <- data.frame("ItemCode" =my_data$ItemCode, "TransactionId" =my_data$TransactionId)
 head(d_fromcsv)
---
+++ path_value.R
@@ -0,0 +1,3 @@
+
+path <- "C:\\Users\\Public\\Documents\\Data\\transIDitemCODE.csv"
+rawDataPath <- "C:\\Users\\Public\\Documents\\OLD\\archive\\transaction_data.csv"

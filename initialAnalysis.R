78d29e63d2fa4050c1a11f2cb2747e138f056f42
XtianLors <crisdalori@gmail.com>
2024-06-27 19:02:15 -0600

    Identify number of transactions with same Transaction ID is in the db.

--- initialAnalysis.R
+++ initialAnalysis.R
@@ -1,13 +1,17 @@
 # Autor: López Rios Cristian Daniel
 # 11 de Marzo de 2024
 
-#list.files(path=".")
+list.files(path=".")
 #install.packages("arules")
 
 library(arules)
 library(dplyr)
+# In order to source we should be in MarketBasketAnalysisExample
 source("./path_value.R")
 
+#This function helps to identify the data structure and standards used in the data set.
+   #This function belongs to data mining since sometimes data doesn't load in a standard way.
+   #It is meant to help identify data inconsistencies and format.
 readerOfData <- function(path, spitter = FALSE )
 {
 
@@ -23,7 +27,7 @@ readerOfData <- function(path, spitter = FALSE )
    }
    return(paste(values, head(insiderData), tail(insiderData))) 
 }
-readerOfData(rawDataPath, spitter = TRUE)
+#readerOfData(rawDataPath, spitter = TRUE)
 
 full_data <- read.csv(rawDataPath)
 typeof(full_data)
@@ -52,8 +56,26 @@ wholeRetailData <- as_tibble(full_data)
 # 
 # retailData <- as_tibble(my_data)
 
-#Use tibble (dplyr) functions to summarize the appropriate data set for initial
+# Use tibble (dplyr) functions to summarize the appropriate data set for initial
    # analysis.
+# We should focus on a few variables. Since we care about transactions, lets only
+   #use transaction ID and product ID.
+
+View(wholeRetailData)
+gather()
+
+newRetailData <- wholeRetailData %>%
+                    select(TransactionId, ItemDescription, NumberOfItemsPurchased, CostPerItem)
+
+# Checking for the retail data gathered
+newRetailData %>% 
+   distinct(TransactionId)
+
+newRetailData %>%
+   group_by(TransactionId) %>%
+   summarize(order.count = n_distinct(TransactionId))
+
+
 
 summarize(group_by(retailData, "TransactionId"), order.count = n_distinct(TransactionId))
 
@@ -62,5 +84,17 @@ summarize(group_by(retailData, "ItemCode"), order.count = n_distinct(ItemCode))
 d_fromcsv <- data.frame("ItemCode" =my_data$ItemCode, "TransactionId" =my_data$TransactionId)
 head(d_fromcsv)
 write.csv(d_fromcsv, "transIDitemCODE.csv")
+
+#Weighted Transactions
+
+
+# Web App
+
+
+
+
+
+
+
 
 
